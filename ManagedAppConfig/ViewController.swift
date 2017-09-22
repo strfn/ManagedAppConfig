bbe0a86126a2b4580a63df9e777f549419e93de5
Michele Simone <michele.si@gmail.com>
2017-09-22 23:11:58 +0100

    xcode project

---
+++ ManagedAppConfig/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ManagedAppConfig
+//
+//  Created by Michele_personal on 22/09/2017.
+//  Copyright © 2017 Michele Simone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
